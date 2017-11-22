1b54813d82fdb44efaae6a632aaad152313950c3
Yash Kulshrestha <yash.kulshrestha@gmail.com>
2017-11-21 17:19:12 -0800

    and we're done here

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 require = require('@std/esm')(module); // eslint-disable-line no-global-assign
+const chalk = require('chalk');
 
+console.log(chalk.yellow('From JS -'));
 require('./js/tests.js');
+
+console.log(chalk.cyan('\n\nFrom TS -'));
 require('./ts/tests.js');
